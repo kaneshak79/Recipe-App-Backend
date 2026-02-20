e74b9a69348ffa304368a23fe9a46c932614bb21
Kanesha K <kaneshak79@gmail.com>
2026-02-20 12:31:19 +0530

    Uncomment connectDB function in config.js
    Uncommented the connectDB function and its import statement.

--- database/config.js
+++ database/config.js
@@ -1,16 +1,3 @@
-// import mongoose from "mongoose";
-
-// export const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGODB);
-//     console.log("Connected to MONGODB");
-//   } catch (error) {
-//     console.error("MongoDB Connection Failed:", error.message);
-//   }
-// };
-
-// // module.exports=connectDB;
-
 import mongoose from "mongoose";
 
 const connectDB = async () => {
