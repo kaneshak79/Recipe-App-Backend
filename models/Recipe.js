6bce12781cf11bfa3a43d00141a34634eb9da5d2
Kanesha K <kaneshak79@gmail.com>
2026-02-20 12:31:54 +0530

    Clean up Recipe.js by removing commented code
    Removed commented-out code for recipe schema definition.

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,35 +1,3 @@
-// // const mongoose = require("mongoose");
-// import mongoose from "mongoose";
-
-// const recipeSchema = new mongoose.Schema({
-//   title: {
-//     type: String,
-//     required: true
-//   },
-//   description: {
-//     type: String
-//   },
-//   ingredients: {
-//     type: [String],
-//     required: true
-//   },
-//   instructions: {
-//     type: String,
-//     required: true
-//   },
-//   cookingTime: {
-//     type: Number
-//   },
-//   createdAt: {
-//     type: Date,
-//     default: Date.now
-//   }
-// });
-
-// // module.exports = mongoose.model("Recipe", recipeSchema);
-// const recipe=mongoose.model("Recipe", recipeSchema);
-// export default recipe;
-
 import mongoose from "mongoose";
 
 const recipeSchema = new mongoose.Schema({
